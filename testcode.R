f5e131fefe6d0ac2303940f7719960570a8f7aa7
Brian OMeara <omeara.brian@gmail.com>
2016-07-02 17:31:02 -0400

    go for verbose output

--- testcode.R
+++ testcode.R
@@ -5,13 +5,13 @@ tree<-read.tree('example/rokas.yeast.red.tre')
 time.1 <- system.time(result.1 <- SelacLargeOptimize(codon.data.path = 'example/', phy = tree,
              edge.length = 'optimize', optimal.aa = 'optimize', data.type='codon',
              nuc.model = "GTR", include.gamma = FALSE, ncats = 4, numcode = 1,
-             diploid = FALSE, k.levels = 0, aa.properties = NULL, verbose = FALSE,
+             diploid = FALSE, k.levels = 0, aa.properties = NULL, verbose = TRUE,
              n.cores = 2, max.restarts = 2, n.partitions=2))
 
 time.2 <- system.time(result.2 <- SelacLargeOptimize(codon.data.path = 'example/', phy = tree,
                           edge.length = 'optimize', optimal.aa = 'optimize', data.type='codon',
                           nuc.model = "GTR", include.gamma = FALSE, ncats = 4, numcode = 1,
-                          diploid = FALSE, k.levels = 0, aa.properties = NULL, verbose = FALSE,
+                          diploid = FALSE, k.levels = 0, aa.properties = NULL, verbose = TRUE,
                           n.cores = 2, max.restarts = 2, n.partitions=2, tol.step=2))
 
 
