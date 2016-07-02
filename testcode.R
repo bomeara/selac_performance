5cc7ac0282988bbaa39383d50286c3c0f8ccdc2b
Brian OMeara <omeara.brian@gmail.com>
2016-07-02 17:30:19 -0400

    fixed an incorrect argument

--- testcode.R
+++ testcode.R
@@ -6,13 +6,13 @@ time.1 <- system.time(result.1 <- SelacLargeOptimize(codon.data.path = 'example/
              edge.length = 'optimize', optimal.aa = 'optimize', data.type='codon',
              nuc.model = "GTR", include.gamma = FALSE, ncats = 4, numcode = 1,
              diploid = FALSE, k.levels = 0, aa.properties = NULL, verbose = FALSE,
-             n.cores = 2, max.restarts = 1, parallel.type='by.gene', n.partitions=2))
+             n.cores = 2, max.restarts = 2, n.partitions=2))
 
 time.2 <- system.time(result.2 <- SelacLargeOptimize(codon.data.path = 'example/', phy = tree,
                           edge.length = 'optimize', optimal.aa = 'optimize', data.type='codon',
                           nuc.model = "GTR", include.gamma = FALSE, ncats = 4, numcode = 1,
                           diploid = FALSE, k.levels = 0, aa.properties = NULL, verbose = FALSE,
-                          n.cores = 2, max.restarts = 1, parallel.type='by.gene', n.partitions=2, tol.step=2))
+                          n.cores = 2, max.restarts = 2, n.partitions=2, tol.step=2))
 
 
 save(list=ls(),file='exampleRun.Rdata')
