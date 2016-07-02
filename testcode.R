15258c3d663a3827ac9ebe50463931facf13ba82
Brian O'Meara <omeara.brian@gmail.com>
2016-07-02 17:26:49 -0400

    set up for initial run

---
+++ testcode.R
@@ -0,0 +1,21 @@
+library(selac)
+tree<-read.tree('example/rokas.yeast.red.tre')
+
+
+time.1 <- system.time(result.1 <- SelacLargeOptimize(codon.data.path = 'example/', phy = tree,
+             edge.length = 'optimize', optimal.aa = 'optimize', data.type='codon',
+             nuc.model = "GTR", include.gamma = FALSE, ncats = 4, numcode = 1,
+             diploid = FALSE, k.levels = 0, aa.properties = NULL, verbose = FALSE,
+             n.cores = 2, max.restarts = 1, parallel.type='by.gene', n.partitions=2))
+
+time.2 <- system.time(result.2 <- SelacLargeOptimize(codon.data.path = 'example/', phy = tree,
+                          edge.length = 'optimize', optimal.aa = 'optimize', data.type='codon',
+                          nuc.model = "GTR", include.gamma = FALSE, ncats = 4, numcode = 1,
+                          diploid = FALSE, k.levels = 0, aa.properties = NULL, verbose = FALSE,
+                          n.cores = 2, max.restarts = 1, parallel.type='by.gene', n.partitions=2, tol.step=2))
+
+
+save(list=ls(),file='exampleRun.Rdata')
+system("git add exampleRun.Rdata")
+system("git commit -m'run done' -a")
+system("git push")
